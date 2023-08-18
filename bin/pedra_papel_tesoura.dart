84c939be6547c3bb947bbb206d39c8e8a4620c37
Rodrigo Bossini <professorbossini@gmail.com>
2023-08-18 18:15:48 -0300

    pedra papel tesoura, apenas comentários, completar com codigo do jogo

---
+++ bin/pedra_papel_tesoura.dart
@@ -0,0 +1,36 @@
+//importar pacote para geração de valores aleatórios
+//importar pacote para captura de dados da entrada padrão
+
+//criar um enum que representa as opções
+//pedra, papel, tesoura, sair
+
+void jogo(){
+
+  //loop infinito ou três rodadas (fazer um melhor de três)
+  //exibir menu
+
+  //capturar a opção do usuário e validar, intervalo de 1 a 4
+
+  //se o usuário digitou 4, sair
+
+  //senão
+
+  //sortear a jogada do computador
+
+  //mapear opção do usuário, de int para enum (1-papel, 2-tesoura ...)
+  //mapear opção do computador, de int para enum..
+
+  //exibir as opções de cada um
+  //você (pedra) vs (tesoura) computador
+
+  //decidir se houve vencedor ou empate e mostrar o resultado
+
+  //aguardar 5 segundos para dar tempo de ver o resultado
+
+  //exibir ************************************************
+
+}
+
+void main(){
+  jogo();
+}
